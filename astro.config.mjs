f17e21dff751a1a967178c45af3b907eaefc700b
Abhishek Saha <mr.abhisheksaha@gmail.com>
2022-08-01 10:16:41 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  site: "sahaabhishek.github.io",
+  site: "https://sahaabhishek.github.io",
   base: "/my-blog",
   vite: {
     ssr: {
