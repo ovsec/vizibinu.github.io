e12e3f244f83b8d9e46b77c93427213fddd1dd9f
Abhishek Saha <mr.abhisheksaha@gmail.com>
2022-08-03 13:05:50 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import svelte from "@astrojs/svelte";
+
+//import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+ 
   integrations: [tailwind(), svelte()],
   site: "https://sahaabhishek.github.io",
-  base:  "/my-blog",
   vite: {
     ssr: {
       noExternal: ['three', 'troika-three-text', 'geolib']
     }
+  },
+  legacy: {
+    astroFlavoredMarkdown: true
   }
 });
