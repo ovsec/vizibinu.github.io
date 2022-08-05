cf3b1a8b571dc00270365bf983d12cddba5f947f
sahaabhishek <sahaabhishek@users.noreply.github.com>
2022-08-05 10:04:30 +0000

    deploy: b9dd3665ca7056381cf4365650993548148f64b6

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/line-clamp'),
-	],
-};
