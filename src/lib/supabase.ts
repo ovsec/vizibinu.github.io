25f10868e372ab1d79550eac19c909d652c236f2
Abhishek Saha <mr.abhisheksaha@gmail.com>
2023-10-25 18:40:01 +0200

    Layout change

---
+++ src/lib/supabase.ts
@@ -0,0 +1,19 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const submitContact = async (user: any) => {
+
+
+    const { data, error } = await supabase
+        .from('contacts')
+        .insert([
+            { email: user.email, name: user.name },
+        ])
+        .select()
+    
+
+}
